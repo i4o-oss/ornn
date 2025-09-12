881eae585f03e3e95fe63bfdbee685658669522a
Ilango Rajagopal <rajagopalilango@gmail.com>
2025-09-12 17:59:20 +0530

    Update create command flags

--- cmd/create.go
+++ cmd/create.go
@@ -35,14 +35,14 @@ func init() {
 	})
 
 	// File-based configuration
-	CreateCmd.Flags().StringVar(&config.ConfigurationFile, "config", "", "config file (default is $HOME/.ornn.toml)")
+	CreateCmd.Flags().StringVar(&config.ConfigurationFile, "config", "$HOME/.ornn.toml", "config file location")
 
 	// Flag-based configuration
 	CreateCmd.Flags().StringVar(&configuration.Cache, "cache", "", "Redis / Memcached / Valkey")
 	CreateCmd.Flags().StringVar(&configuration.Database, "database", "PostgreSQL", "PostgreSQL / SQLite")
 	CreateCmd.Flags().StringVar(&configuration.FileStorage, "file-storage", "", "Cloudflare R2 / AWS S3")
 	CreateCmd.Flags().StringVar(&configuration.Queue, "queue", "", "Redis / AWS SQS")
-	CreateCmd.Flags().StringVar(&configuration.RateLimiting, "rate-limiting", "", "In-memory / Redis / Memcached / Valkey")
+	CreateCmd.Flags().StringVar(&configuration.RateLimiting, "rate-limiting", "In-memory", "In-memory / Redis / Memcached / Valkey")
 	CreateCmd.Flags().StringVar(&configuration.Router, "router", "net/http", "BunRouter / Chi / Echo / Fiber / Gin / Gorilla Mux / net/http")
 	CreateCmd.Flags().BoolVar(&configuration.Tasks, "tasks", false, "Set up Tasks")
 	CreateCmd.Flags().BoolVar(&configuration.Scheduler, "scheduler", false, "Set up Scheduler")
